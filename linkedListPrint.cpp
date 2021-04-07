5302df6a950ddc166485477087a56b5027734213
Akash Kumar <65735040+Akash671@users.noreply.github.com>
2021-04-07 22:43:31 +0530

    geeksforgeeks problem solution
    LinkedList

---
+++ linkedListPrint.cpp
@@ -0,0 +1,89 @@
+/* author :  @akash kumar  */
+
+
+
+#include <iostream>
+#include<cstdio>
+#include<cstdlib>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*start;
+
+void insert();
+
+/*
+  Print elements of a linked list on console 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     Node *next;
+  }
+*/
+
+class Solution
+{
+    public:
+    void display(Node *head)
+    {
+      //your code goes here
+      struct Node *ptr;
+      ptr=head;
+      while(ptr!=NULL)
+      {
+          cout<<ptr->data<<" ";
+          ptr=ptr->next;
+      }
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+      start=NULL;
+      insert();
+      Solution ob;
+      ob.display(start);
+      cout<<endl;
+    }
+    return 0;
+
+}
+
+
+ void insert()
+ {
+     int n,value;
+     cin>>n;
+     struct Node *temp;
+     for(int i=0;i<n;i++)
+     {
+         cin>>value;
+         if(i==0)
+         {
+              start=(struct Node *) malloc( sizeof(struct Node) );
+              start->data=value;
+              start->next=NULL;
+              temp=start;
+              continue;
+         }
+         else
+         {
+             temp->next= (struct Node *) malloc( sizeof(struct Node) );
+             temp=temp->next;
+             temp->data=value;
+             temp->next=NULL;
+         }
+     }
+ }
